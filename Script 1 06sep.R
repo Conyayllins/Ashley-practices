7f468a43116e8cdf63fa9ef6feb963a8559ab250
conyayllins <conyayllins@gmail.com>
2019-09-06 21:03:02 -0400

    Script clase 06 septiembre

---
+++ Script 1 06sep.R
@@ -0,0 +1,30 @@
+#install.packages("ggplot2")
+
+library("ggplot2")
+
+dat<- data.frame(
+  time = factor( c("lunch","dinner"), levels=c("lunch","dinner")),
+  total_bill= c(14.89, 17.23)
+  )
+dat
+
+ggplot(data=dat, aes(x=time, y=total_bill)) +
+  geom_bar(stat="identity")
+
+A<-2
+A
+A<-3
+A
+A<-3
+A
+B<-5
+A < B
+A > B
+C <- "Profesorsita"
+C
+D <- "Diana"
+D
+D > C
+D>C
+D<C
+!(TRUE)
